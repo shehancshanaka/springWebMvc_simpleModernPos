76e05b0c12fd83eb32217e2f0896241f8b2e2a0c
shehan <shehaanc@gmail.com>
2019-06-21 12:42:49 +0530

    complete the project

---
+++ SpringBackend/Backend/src/main/java/lk/ijse/dep/controller/OrderController.java
@@ -0,0 +1,56 @@
+package lk.ijse.dep.controller;
+
+import lk.ijse.dep.dto.OrderDTO;
+import lk.ijse.dep.dto.OrderDetailsDTO;
+import lk.ijse.dep.dto.OrderItemDetails;
+import lk.ijse.dep.service.custom.OrderBO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RequestMapping("api/v1/orders")
+@RestController
+public class OrderController {
+
+    @Autowired
+    OrderBO orderBO;
+
+    @GetMapping(params ="maxid=true", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> currentID(){
+
+        String id = orderBO.getCurrentId();
+        System.out.println(id);
+        return new ResponseEntity<String>("\""+id+"\"",HttpStatus.OK);
+    }
+
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<OrderDTO> getAllOrders(){
+        return orderBO.getAllOrders();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveOrder(@RequestBody OrderDTO orderDTO){
+        OrderDetailsDTO detailsDTO = orderDTO.getDetailsDTO();
+        List<OrderItemDetails> orderItemDTOS = orderDTO.getOrderItemDTOS();
+        System.out.println(detailsDTO.getOrderid());
+
+        if (orderBO.orderIsExists(orderDTO.getDetailsDTO().getOrderid())){
+            return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+        }
+        else {
+            if (detailsDTO.getCusid().isEmpty() || detailsDTO.getOrderdate().isEmpty() || detailsDTO.getOrderid().isEmpty() || orderItemDTOS.size()==0){
+                return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+            }
+            else {
+                orderBO.finalOrderPalcement(detailsDTO,orderItemDTOS);
+                return new ResponseEntity<Void>(HttpStatus.CREATED);
+            }
+        }
+    }
+
+}
